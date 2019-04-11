253809e9bcb794d9bf0f82a167ac968e9bce98da
Владимир <vovakalini4ev@gmail.com>
2019-04-11 16:34:50 +0300

    Добавьте файлы проекта.

---
+++ Task_03/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskThree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> ActionType = new List<string>
+            {
+                "Create",
+                "Read",
+                "Update",
+                "Delete"
+            };
+            string Search = ActionType.Find((x) => x == "Create");
+            Console.WriteLine(Search);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/*using System;
+namespace TaskThree
+{
+
+    /// Задача - перепишите данный код так, чтобы он работал через коллекции C#, вместо конструкции switch
+
+
+    public enum ActionType
+    {
+        Create,
+
+        Read,
+
+        Update,
+
+        Delete
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = ActionType.Read;
+            if (type == ActionType.Create)
+                CreateMethod(type);
+            else if (type == ActionType.Read)
+                ReadMethod(type);
+            else if (type == ActionType.Update)
+                UpdateMethod(type);
+            else if (type == ActionType.Delete)
+                DeleteMethod(type);
+        }
+        private static void CreateMethod(ActionType type)
+        {
+            Console.WriteLine(type.ToString());
+            Console.ReadKey();
+        }
+
+        private static void ReadMethod(ActionType type)
+        {
+            Console.WriteLine(type.ToString());
+            Console.ReadKey();
+        }
+
+        private static void UpdateMethod(ActionType type)
+        {
+            Console.WriteLine(type.ToString());
+            Console.ReadKey();
+        }
+
+        private static void DeleteMethod(ActionType type)
+        {
+            Console.WriteLine(type.ToString());
+            Console.ReadKey();
+        }
+    }
+} */
